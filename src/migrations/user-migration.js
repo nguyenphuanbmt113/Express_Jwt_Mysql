cd49f91bf31bc036b9ebf771b62c1201edc37388
nguyenphuanbmt113 <nguyenphuanbmt113@gmail.com>
2022-07-09 19:41:50 +0700

    set up connection orm

--- src/migrations/user-migration.js
+++ src/migrations/user-migration.js
@@ -26,6 +26,9 @@ module.exports = {
       phone: {
         type: Sequelize.STRING,
       },
+      groupId: {
+        type: Sequelize.INTEGER,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
